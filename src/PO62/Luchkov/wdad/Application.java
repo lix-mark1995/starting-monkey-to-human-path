ffa69f46580a1c5f22d71cd419f7cb586b407eb4
lix-mark1995 <artyom.luchkov2013@gmail.com>
2018-09-27 11:01:28 +0400

    Added entry point

--- src/PO62/Luchkov/wdad/Application.java
+++ src/PO62/Luchkov/wdad/Application.java
@@ -1,7 +1,7 @@
 package PO62.Luchkov.wdad;
-//blablabla
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("I’m Arteom Luchkov, and I’m not a monkey");
+        System.out.println("I’m Artyom Luchkov, and I’m not a monkey");
     }
 }
